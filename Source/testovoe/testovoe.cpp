17527ef81f69525a31d65b6f9f4553164a771a13
plushish <all.Kriht@yandex.ru>
2023-08-05 01:10:44 +0300

    Initial commit

---
+++ Source/testovoe/testovoe.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "testovoe.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, testovoe, "testovoe" );
---
+++ Source/testovoe/testovoeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "testovoeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTOVOE_API AtestovoeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
